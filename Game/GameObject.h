c9037a835a19368dc5dada31d9461462e12d7ca3
拠点斗 <kyotento0507@gmail.com>
2019-07-12 14:47:44 +0900

    GameObject始動！

---
+++ Game/GameObject.h
@@ -0,0 +1,32 @@
+/*!
+ *@brief	ゲームオブジェクトの基底クラス。
+ */
+
+#pragma once
+
+#define _CGAMEOBJECT_H_
+
+class GameObject : Noncopyable
+{
+
+public:
+	GameObject();
+	~GameObject();
+
+	//返り値をしっかりと返す。
+	virtual bool Start()
+	{
+		return true;
+	}
+
+	//Update関数。
+	virtual void Update()
+	{
+
+	}
+
+protected:
+
+
+};
+
