b8d15533c0f8ba3f15d2e9ba3662c797e0745162
拠点斗 <kyotento0507@gmail.com>
2019-08-26 09:22:30 +0900

    Draw仮

--- Game/Sprite.cpp
+++ Game/Sprite.cpp
@@ -250,7 +250,36 @@ void Sprite::Init(const wchar_t* TextureFilePath, float w, float h)
 
 //描画処理。
 void Sprite::Draw()
-{
+{		
+	//デバイスコンテキストを取得。
+	auto deviceContext = g_graphicsEngine->GetD3DDeviceContext();
+	//デバイスコンテキストに描画コマンドを積む。
+	//頂点バッファを設定。
+	unsigned int stride = sizeof(SVertex);	//頂点バッファのストライド
+	unsigned int offset = 0;						//頂点バッファのオフセット。
+	deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+	//プリミティブのトポロジーを設定。
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	//頂点シェーダーを設定。
+	deviceContext->VSSetShader(
+		(ID3D11VertexShader*)m_vsShader.GetBody(),	//頂点シェーダー。
+		NULL,										//NULLでいい。
+		0											//0でいい。
+	);
+	//ピクセルシェーダーを設定。
+	deviceContext->PSSetShader(
+		(ID3D11PixelShader*)m_psShader.GetBody(),	//ピクセルシェーダー。
+		NULL,										//NULLでいい。
+		0											//0でいい。
+	);
+	//頂点レイアウトを設定。
+	deviceContext->IASetInputLayout(m_vsShader.GetInputLayout());
+
+	//ここまで設定した内容でドロー
+	deviceContext->Draw(
+		3,	//頂点数。
+		0	//描画開始の頂点番号。大抵０。
+	);
 
 
 }
